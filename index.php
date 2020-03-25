2321ee4e9778eec730e313ff44930c1839a368f9
Emerson Oliveira <emerson-ods@hotmail.com>
2020-03-25 18:44:41 -0300

    update index.php
    inclusion of the phone mask

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
 <html>
 <head>
 	<title>Teste de Integração Agendor</title>
+
+	<script type="text/javascript">
+        function mascara(o,f){
+            v_obj=o
+            v_fun=f
+            setTimeout("execmascara()",1)
+        }
+        function execmascara(){
+            v_obj.value=v_fun(v_obj.value)
+        }
+        function mtel(v){
+            
+            v=v.replace(/\D/g,"");             
+            v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+            v=v.replace(/(\d)(\d{4})$/,"$1-$2");    
+            return v;
+        }
+        function id( el ){
+            return document.getElementById( el );
+        }
+        window.onload = function(){
+            id('phone').setAttribute('maxlength',15);
+            id('phone').onkeypress =
+            function(){
+                mascara( this, mtel );
+            }
+        }
+	</script>
 </head>
 <body>
 
@@ -24,7 +52,7 @@
 		</div>  
 		<div class="form-group">
 		    <label for="name">Telefone:</label>
-	 		<input type="text" size="35" maxlength="15" minlength="14" name="phone" placeholder="(xx) xxxx-xxxx"> 
+	 		<input type="text" id="phone" size="35" maxlength="15" minlength="14" name="phone" placeholder="(xx) xxxx-xxxx"> 
 		</div> 
 
 		<div class="form-group">
